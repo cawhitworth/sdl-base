7741f9bd651dc63ebdad2861ac5f2a953932a54b
Chris Whitworth <chris.whitworth@red-gate.com>
2015-04-14 12:23:10 +0100

    Suppress copy/move on SDLWrapper

--- yarl/SDLWrapper.h
+++ yarl/SDLWrapper.h
@@ -15,6 +15,12 @@ public:
     SDLWrapper(int width, int height);
     ~SDLWrapper();
 
+    // No copying/moving
+    SDLWrapper(const SDLWrapper& wrapper) = delete;
+    SDLWrapper& operator=(const SDLWrapper& wrapper) = delete;
+    SDLWrapper(SDLWrapper&& wrapper) = delete;
+    SDLWrapper& operator=(SDLWrapper&& wrapper) = delete;
+
     const SDLSurface& ScreenSurface() const { return m_screenSurface; }
     const SDLRenderer& Renderer() const { return m_renderer;  }
 
