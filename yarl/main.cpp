7c96327a812328f3adb76d6bfb45ebe1dfce4231
Chris Whitworth <chris.whitworth@red-gate.com>
2015-04-16 12:21:38 +0100

    Add an imp and let him wander. Animation!

--- yarl/main.cpp
+++ yarl/main.cpp
@@ -13,6 +13,11 @@
 #include "Map.h"
 #include "MapRenderer.h"
 
+#include "Imp.h"
+#include "ImpRenderer.h"
+
+using namespace std::chrono;
+
 #pragma warning(disable: 4100)
 int main(int argc, char* argv[])
 {
@@ -20,6 +25,7 @@ int main(int argc, char* argv[])
     try {
         Position origin(0, 0);
         Map m(Size(200, 200));
+        std::vector<Imp> imps;
 
         SDLWrapper wrapper(SCREEN_WIDTH, SCREEN_HEIGHT);
 
@@ -27,12 +33,16 @@ int main(int argc, char* argv[])
         auto textRenderer = TextRenderer("images/Teeto_K_18x18.PNG", renderer);
         auto charSize = textRenderer.CharSize();
         MapRenderer mapRenderer(textRenderer, m);
+        ImpRenderer impRenderer(textRenderer);
 
         Size viewPort(SCREEN_WIDTH / charSize.w, SCREEN_HEIGHT / charSize.h);
 
         FPS fps;
 
         AddDungeonHeart(m, Position(8, 8));
+        imps.push_back(Imp(m, Position(5, 5)));
+
+        auto last = high_resolution_clock::now();
 
         auto quit = false;
         while (!quit)
@@ -50,8 +60,16 @@ int main(int argc, char* argv[])
 
             renderer.Clear();
 
-
             mapRenderer.Render(origin, viewPort);
+
+            auto elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - last);
+            last = high_resolution_clock::now();
+
+            for (auto& i : imps)
+            {
+                i.Update(elapsed);
+                impRenderer.Render(i);
+            }
 
             std::stringstream fpsText;
             fpsText << fps.Fps() << "FPS";
